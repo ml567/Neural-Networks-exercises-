ea8131e18387dd6385caefa3d58b1a4aba822138
ml567 <miyunilowe5@gmail.com>
2026-01-07 18:48:42 +0530

    Update 1p_Neuron_code.py

--- 1p_Neuron_code.py
+++ 1p_Neuron_code.py
@@ -1,3 +1,7 @@
 input = [1.2,5.1, 2.1]
-weights = [3.1, 21.1, 8.7]
+weights = [3.1, 2.1, 8.7]
 bias = 3
+
+
+output = input[0]* weights[0] + input[1]* weights[1] + input[2]* weights[2] + bias
+print(output)
